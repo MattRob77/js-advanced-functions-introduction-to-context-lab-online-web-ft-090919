3a63a7d4c574c6f7b0347bb2136339ef98540ba5
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-07 13:52:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,5 +46,6 @@ function hoursWorkedOnDate (employee, date) {
 } 
 
 function wagesEarnedOnDate (employee, date) { 
-  let hours = hoursWorkedOnDate(employee, date)
+  let hours = hoursWorkedOnDate(employee, date), 
+  
 } 
