0e5d97296e824e1a897a67a5b1b6daae8df3b0e2
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-07 13:49:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+function createEmployeeRecord(arr) { 
+  return { 
+    firstName: arr[0],
+    familyName: arr [1],
+    title: arr [2],
+    payPerHour: arr [3],
+    timeInEvents: [], 
+    timeOutEvents: []
+  } 
+} 
+
+function createEmployeeRecords(arr) {
+    let newArr = []
+    arr.map((employee) => {
+        newArr.push(createEmployeeRecord(employee))
+  })
+  return newArr
+}
+
+function createTimeInEvent (employee, dateStamp) {
+    let timeIn = {
+        type: "TimeIn",
+        hour: parseInt(dateStamp.split(" ")[1]),
+        date: dateStamp.split(" ")[0]
+  }
+  employee.timeInEvents.push(timeIn)
+  return employee
+}
+
+function createTimeOutEvent (employee, dateStamp) { 
+  let timeOut = { 
+    type: "TimeOut", 
+    hour: parseInt(dateStamp.split(" ")[1]),
+    date: dateStamp.split(" ")[0]
+  } 
+  employee.timeOutEvents.push(timeOut)
+  return employee 
+} 
+
+function hoursWorkedOnDate (employee, date) {
+    let startHour = employee.timeInEvents.find(event => event.date === date).hour
+    let endHour = employee.timeOutEvents.find(event => event.date === date).hour
+    
+    return hours
+} 
